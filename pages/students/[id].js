60ab3615aca6747fba182aaf5c843087caea1a6b
imranyy <imranmatano6@gmail.com>
2022-11-25 22:56:09 +0300

    dynamic route 2

--- pages/students/[id].js
+++ pages/students/[id].js
@@ -1,3 +1,16 @@
+export const getStaticPaths=async()=>{
+    const res =await fetch('https://jsonplaceholder.typicode.com/users');
+    const data= await res.json();
+    const paths=data.map(std=>{
+        return{
+            params:{id: std.id.toString()}
+        }
+    })
+    return{
+        paths,
+        fallback:false
+    }
+}
 const Details=()=>{
     return(
         <>
