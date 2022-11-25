fe1f870e1032c9d357abd9af1addf2ae32be79c7
imranyy <imranmatano6@gmail.com>
2022-11-25 21:21:02 +0300

    fetching data using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+//fetches data
+export const getStaticProps=async()=>{
+    const url='https://jsonplaceholder.typicode.com/users';
+    const response=await fetch(url);
+    const parseRes=await response.json();
 
-export default function Home() {
+    return {
+      props:{students: parseRes}
+    }
+ 
+}
+export default function Home({students}) {
   return (
    <>
     <Head>
       <title>Ninja List | Home</title>
       <metadata name='keywords' content='ninjas'/>
-      <link type='favicon' href='/hecker.ico'/>
+      <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon"/>
     </Head>
+    <Image src='/hecker.ico' width={128} height={128} alt='logo'/>
     <h1 className={styles.header}>Homepage</h1>
     <p>This is the first page</p>
+    {students.map((student)=>(
+      <div key={student.id}>
+        <p>{student.name}</p>
+      </div>
+    ))}
    </>
   )
 }
