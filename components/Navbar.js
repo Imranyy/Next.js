21b1fae5abacc145e40b136ca8f530d1aa8b10a2
imranyy <imranmatano6@gmail.com>
2022-11-25 19:59:57 +0300

    using static files like images in nextjs

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar=()=>{
     return(
         <nav>
-           <div className="logo"><h1>Ninjas List</h1></div>
+           <div className="logo"><h1><img width={50} height={50} src='/hecker.ico' alt='logo'/></h1></div>
            <Link href='/'>Home</Link>
            <Link href='/about'>About</Link>
            <Link href='/students'>Student Listing</Link>
