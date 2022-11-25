7302dff125e93b950137e4e14832bab883c82a22
imranyy <imranmatano6@gmail.com>
2022-11-25 20:02:52 +0300

    using nextjs image component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import Image from 'next/image';
 
 const Navbar=()=>{
     return(
         <nav>
-           <div className="logo"><h1><img width={50} height={50} src='/hecker.ico' alt='logo'/></h1></div>
+           <div className="logo"><h1><Image width={50} height={50} src='/hecker.ico' alt='logo'/></h1></div>
            <Link href='/'>Home</Link>
            <Link href='/about'>About</Link>
            <Link href='/students'>Student Listing</Link>
