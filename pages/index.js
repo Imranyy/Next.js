6c42c6ee1fa63887e7be1b84c85ae414f184e5bc
imranyy <imranmatano6@gmail.com>
2022-11-25 21:50:30 +0300

    fetching data :nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-//fetches data
-export const getStaticProps=async()=>{
-    const url='https://jsonplaceholder.typicode.com/users';
-    const response=await fetch(url);
-    const parseRes=await response.json();
 
-    return {
-      props:{students: parseRes}
-    }
- 
-}
-export default function Home({students}) {
+export default function Home() {
   return (
    <>
     <Head>
@@ -23,11 +13,7 @@ export default function Home({students}) {
     <Image src='/hecker.ico' width={128} height={128} alt='logo'/>
     <h1 className={styles.header}>Homepage</h1>
     <p>This is the first page</p>
-    {students.map((student)=>(
-      <div key={student.id}>
-        <p>{student.name}</p>
-      </div>
-    ))}
+    
    </>
   )
 }
--- pages/students/index.js
+++ pages/students/index.js
@@ -1,6 +1,21 @@
 import Head from "next/head";
+import styles from '../../styles/Students.module.css'
+//fetches data
+export const getStaticProps=async()=>{
+    try {
+        const url='https://jsonplaceholder.typicode.com/users';
+        const response=await fetch(url);
+        const parseRes=await response.json();
 
-const Students=()=>{
+        return {
+        props:{students: parseRes}
+        }
+    } catch (error) {
+        console.log(error)
+    }
+ 
+}
+const Students=({students})=>{
     return(
         <>
         <Head>
@@ -8,7 +23,12 @@ const Students=()=>{
             <metadata name='keywords' content='ninjas'/>
             <link type='favicon' href='/hecker.ico'/>
         </Head>
-            <h1>Students</h1>
+            <h1>All Students</h1>
+            {students.map((student)=>(
+                <div key={student.id}>
+                    <a> <h3 className={styles.single}>{student.name}</h3></a>
+                </div>
+            ))}
         </>
     )
 }
